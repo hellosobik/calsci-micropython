c7b38c3594691f8bbe008d6a135aa03025adb7d9
hellosobik <hellosobik@gmail.com>
2024-08-15 23:05:48 +0530

    nav logic done, new_display_buffer_logic

---
+++ new_display_buffer_logic.py
@@ -0,0 +1,33 @@
+menu_buffer=[0,1,2,3,4,5,6,7,8,9]
+menu_buffer_cursor=0
+rows=3
+display_buffer_position=0
+display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows]
+display_buffer_cursor=0
+
+while True:
+    #global menu_buffer_cursor, menu_buffer, rows, display_buffer_position, display_buffer, display_buffer_cursor 
+    #print(display_buffer, menu_buffer_cursor, display_buffer_cursor)
+    for i in display_buffer:
+        row=""
+        if i==menu_buffer_cursor:
+            row+="> "+str(i)
+        else:
+            row+="  "+str(i)
+        print(row)
+    direction=input("Enter the direction: ")
+    if direction=="d":
+        menu_buffer_cursor+=1
+        if menu_buffer_cursor == len(menu_buffer):
+            menu_buffer_cursor=0
+            display_buffer_position=0
+            display_buffer_cursor=0
+        elif menu_buffer_cursor <= display_buffer[-1]:
+            display_buffer_cursor+=1
+        elif menu_buffer_cursor >= display_buffer[-1]:
+            display_buffer_position+=1
+        display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows]
+
+    #print(display_buffer, menu_buffer_cursor, display_buffer_cursor)
+
+        
