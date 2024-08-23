0b1e7716cd775bbe58146cc17f7cf1b639fee04f
hellosobik <hellosobik@gmail.com>
2024-08-24 00:56:05 +0530

    back space complete

--- new_display_buffer_logic.py
+++ new_display_buffer_logic.py
@@ -1,7 +1,7 @@
 import os
-
 # menu_buffer=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 text_buffer="Mechanical engineering designs, analyzes, and improves systems."
+# text_buffer="nagesh"
 menu_buffer_size=len(text_buffer)
 menu_buffer=list(range(menu_buffer_size))
 # menu_buffer=["angle", "fun", "consts", "equations", "search", "cloud", "wifi", "settings"]
@@ -10,8 +10,6 @@ rows=3
 cols=12
 display_buffer_position=0
 display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows*cols]
-# display_buffer_cursor=0
-# menu_buffer_end=len(text_buffer)-1
 no_last_spaces=0
 while True:
     if len(text_buffer)%cols!=0:
@@ -20,16 +18,13 @@ while True:
             text_buffer+=" "
             menu_buffer_size=len(text_buffer)
             menu_buffer=list(range(menu_buffer_size))
-    #global menu_buffer_cursor, menu_buffer, rows, display_buffer_position, display_buffer, display_buffer_cursor 
-    #print(display_buffer, menu_buffer_cursor, display_buffer_cursor)
+    while len(text_buffer)<=display_buffer[-1] or len(text_buffer)<rows*cols:
+        no_last_spaces+=1
+        text_buffer+=" "
+        menu_buffer_size=len(text_buffer)
+        menu_buffer=list(range(menu_buffer_size))
+    display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows*cols]
     os.system('clear')
-    # for i in display_buffer:
-    #     row=""
-    #     if i==menu_buffer_cursor:
-    #         row+="> "+str(i)
-    #     else:
-    #         row+="  "+str(i)
-    #     print(row)
     counter=0
     for i in range(rows):
         print("")
@@ -38,97 +33,51 @@ while True:
             if counter >= len(display_buffer):
                 row+="\t"
             elif display_buffer[counter]==menu_buffer_cursor:
-                row+="  "+str(text_buffer[display_buffer[counter]])+"|"
-                # row+=" >"+str(text_buffer[counter])
+                row+="|"+str(text_buffer[display_buffer[counter]])
             else:
                 row+="  "+str(text_buffer[display_buffer[counter]])
-                # row+="  "+str(text_buffer[counter])
             counter+=1
         print(row)
-
-
     print("")
     text=input("Enter the text: ")
-    if text=="d":
-        menu_buffer_cursor+=cols
+    if text=="d" or text =="r":
+        if text=="d":
+            menu_buffer_cursor+=cols
+        else:
+            menu_buffer_cursor+=1
         if menu_buffer_cursor >= len(menu_buffer)-no_last_spaces:
             menu_buffer_cursor=0
             display_buffer_position=0
-            display_buffer_cursor=0
-
-            # display_buffer_cursor=(menu_buffer_cursor-len(menu_buffer))%cols
-            # display_buffer_position=(menu_buffer_cursor-len(menu_buffer))//cols
-            # menu_buffer_cursor=menu_buffer_cursor-len(menu_buffer)
-
-        # elif menu_buffer_cursor <= display_buffer[-1]:
-        #     # display_buffer_cursor+=cols
-        #     pass
         elif menu_buffer_cursor > display_buffer[-1]:
             display_buffer_position+=cols
-        display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows*cols]
-
-    elif text=="u":
-        menu_buffer_cursor-=cols
-        if menu_buffer_cursor <0:
-            display_buffer_cursor=len(display_buffer)+menu_buffer_cursor
-            # menu_buffer_cursor=len(menu_buffer)+menu_buffer_cursor
-            menu_buffer_cursor=len(menu_buffer)-1
-            if menu_buffer_cursor>=len(menu_buffer)-no_last_spaces:
-                menu_buffer_cursor=len(menu_buffer)-no_last_spaces-1
-            # menu_buffer_cursor=len(menu_buffer)-1
-            display_buffer_position=len(menu_buffer)-rows*cols
-            # display_buffer_position=len(menu_buffer)%cols-(rows-1)*cols
-            # display_buffer_position=len(menu_buffer)-len(menu_buffer)%(rows*cols)+cols-len(menu_buffer)%cols
-
-        # elif menu_buffer_cursor >= display_buffer[0]:
-        #     display_buffer_cursor-=cols
-        elif menu_buffer_cursor < display_buffer[0]:
-            display_buffer_position-=cols
-        display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows*cols]
-    
-    elif text=="r":
-        menu_buffer_cursor+=1
-        if menu_buffer_cursor>=len(menu_buffer)-no_last_spaces:
-            # display_buffer_cursor=(menu_buffer_cursor-len(menu_buffer))%cols
-            # display_buffer_position=(menu_buffer_cursor-len(menu_buffer))//cols
-            display_buffer_position=0
-            # menu_buffer_cursor=menu_buffer_cursor-len(menu_buffer)
-            if menu_buffer_cursor>=len(menu_buffer)-no_last_spaces:
-                menu_buffer_cursor=0
-        elif menu_buffer_cursor>display_buffer[-1]:
-            display_buffer_position+=cols
-            # display_buffer_cursor
-        display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows*cols]
-
-    elif text=="l":
-        menu_buffer_cursor-=1
-        if menu_buffer_cursor<0:
-            # display_buffer_cursor=len(display_buffer)+menu_buffer_cursor
-            # menu_buffer_cursor=len(menu_buffer)+menu_buffer_cursor
+    elif text=="u" or text=="l":
+        if text=="u":
+            menu_buffer_cursor-=cols
+        else:
+            menu_buffer_cursor-=1
+        if menu_buffer_cursor < 0:
             menu_buffer_cursor=len(menu_buffer)-no_last_spaces-1
             display_buffer_position=len(menu_buffer)-rows*cols
-            # if menu_buffer_cursor>=len(menu_buffer)-no_last_spaces:
-            #     menu_buffer_cursor=len(menu_buffer)-no_last_spaces-1
-        elif menu_buffer_cursor<display_buffer[0]:
+        elif menu_buffer_cursor < display_buffer[0]:
             display_buffer_position-=cols
-            # display_buffer_cursor
-        display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows*cols]
-    
+    elif text=="b":
+        if menu_buffer_cursor < 0:
+            menu_buffer_cursor=0
+            display_buffer_position=0
+        elif menu_buffer_cursor < display_buffer[0]:
+            text_buffer=text_buffer[0:menu_buffer_cursor-1]+text_buffer[menu_buffer_cursor:len(text_buffer)]
+            display_buffer_position-=cols
+            menu_buffer_size=len(text_buffer)
+            menu_buffer_cursor-=1
+        elif menu_buffer_cursor > 0 and menu_buffer_cursor>=display_buffer[0]:
+            text_buffer=text_buffer[0:menu_buffer_cursor-1]+text_buffer[menu_buffer_cursor:len(text_buffer)]
+            menu_buffer_size=len(text_buffer)
+            menu_buffer_cursor-=1
     else:
-        text_buffer=text_buffer[0:menu_buffer_cursor+1]+text+text_buffer[menu_buffer_cursor+1:len(text_buffer)]
+        text_buffer=text_buffer[0:menu_buffer_cursor]+text+text_buffer[menu_buffer_cursor:len(text_buffer)]
         menu_buffer_size+=len(text)
         menu_buffer=list(range(menu_buffer_size))
         menu_buffer_cursor+=len(text)
         if menu_buffer_cursor>display_buffer[-1]:
-            # display_buffer_position=((menu_buffer_cursor)//(rows*cols-1))*(rows*cols)
             display_buffer_position=menu_buffer_cursor-menu_buffer_cursor%cols-((rows-1)*cols)
-        display_buffer=menu_buffer[display_buffer_position:display_buffer_position+rows*cols]
-    text_buffer=text_buffer.strip()
-
-        
-
-
-    
-    #print(display_buffer, menu_buffer_cursor, display_buffer_cursor)
-
-        
+    text_buffer=text_buffer.strip()+" "
