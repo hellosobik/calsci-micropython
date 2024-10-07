a3b025263b2ff7a064db7c34034ecebd46562438
hellosobik <103817381+hellosobik@users.noreply.github.com>
2024-10-07 19:48:20 +0530

    graph with resolution scale

--- st7565_NHD-C12864LZ-FSW-FBW-3V3.py
+++ st7565_NHD-C12864LZ-FSW-FBW-3V3.py
@@ -42,7 +42,7 @@ def init_display():
     write_instruction(0x2F)  # Power control: Booster, Regulator, Follower on
     write_instruction(0x27)  # Set contrast (0x27 can be adjusted)
     write_instruction(0x81)  # Set contrast
-    write_instruction(0x16)  # Contrast level (set to 0x16 here)
+    write_instruction(0x0A)  # Contrast level (set to 0x16 here)
     write_instruction(0xAF)  # Display ON
 
 # Pixel setting functions
@@ -89,8 +89,68 @@ clear_display()
 x_past=0
 y_past=0
 
+# graph_letters={
+#     " ":[0b00000000],
+#     "1":[0b00100010,0b00111110,0b00100000],
+#     "2":[0b00110010,0b00101010,0b00101110],
+#     "3":[0b00100010,0b00101010,0b00111110]
+# }
+
+graph_letters = {
+    " ": [0b00000000],  # Space
+    "1": [0b00100010, 0b00111110, 0b00100000],
+    "2": [0b00110010, 0b00101010, 0b00101110],
+    "3": [0b00100010, 0b00101010, 0b00111110],
+    "4": [0b00001110, 0b00001000, 0b00111110],
+    "5": [0b00101110, 0b00101010, 0b00111010],
+    "6": [0b00111110, 0b00101010, 0b00111010],
+    "7": [0b00000010, 0b00111110, 0b00000010],
+    "8": [0b00111110, 0b00101010, 0b00111110],
+    "9": [0b00101110, 0b00101010, 0b00111110],
+    "0": [0b00111110, 0b00100010, 0b00111110],
+
+    "A": [0b00111110, 0b00010010, 0b00111110],  # Letter A
+    "B": [0b00111110, 0b00101010, 0b00110110],  # Letter B
+    "C": [0b00111110, 0b00100010, 0b00100010],  # Letter C
+    "D": [0b00111110, 0b00100010, 0b00111110],  # Letter D
+    "E": [0b00111110, 0b00101010, 0b00101010],  # Letter E
+    "F": [0b00111110, 0b00001010, 0b00001010],  # Letter F
+    "G": [0b00111110, 0b00101010, 0b00111010],  # Letter G
+    "H": [0b00111110, 0b00001000, 0b00111110],  # Letter H
+    "I": [0b00100010, 0b00111110, 0b00100010],  # Letter I
+    "J": [0b00110000, 0b00100000, 0b00111110],  # Letter J
+    "K": [0b00111110, 0b00001000, 0b00110110],  # Letter K
+    "L": [0b00111110, 0b00100000, 0b00100000],  # Letter L
+    "M": [0b00111110, 0b00000100, 0b00111110],  # Letter M
+    "N": [0b00111110, 0b00011000, 0b00111110],  # Letter N
+    "O": [0b00111110, 0b00100010, 0b00111110],  # Letter O
+    "P": [0b00111110, 0b00001010, 0b00001110],  # Letter P
+    "Q": [0b00111110, 0b00110010, 0b00111110],  # Letter Q
+    "R": [0b00111110, 0b00011010, 0b00101110],  # Letter R
+    "S": [0b00101110, 0b00101010, 0b00111010],  # Letter S
+    "T": [0b00000010, 0b00111110, 0b00000010],  # Letter T
+    "U": [0b00111110, 0b00100000, 0b00111110],  # Letter U
+    "V": [0b00111110, 0b00010000, 0b00111110],  # Letter V
+    "W": [0b00111110, 0b00010000, 0b00111110],  # Letter W
+    "X": [0b00110110, 0b00001000, 0b00110110],  # Letter X
+    "Y": [0b00001110, 0b00110000, 0b00001110],  # Letter Y
+    "Z": [0b00110010, 0b00101010, 0b00100110],  # Letter Z
+
+    "-": [0b00001000, 0b00001000, 0b00001000],  # Dash
+    "_": [0b00100000, 0b00100000, 0b00100000],  # Underscore
+    "=": [0b00001010, 0b00001010, 0b00001010],  # Equals sign
+    "+": [0b00001000, 0b00011100, 0b00001000],  # Plus sign
+    ".": [0b00100000],                          # Period
+    ",": [0b00100000, 0b00010000],              # Comma
+    ":": [0b00100100],                          # Colon
+    ";": [0b00100000, 0b00010100],              # Semicolon
+    "!": [0b00111110],                          # Exclamation mark
+    "?": [0b00000010, 0b00101010, 0b00001110],  # Question mark
+}
+
+
 def plot_function(fb, func, x_min, x_max, y_min, y_max, width, height):
-    global x_past, y_past
+    global x_past, y_past, graph_letters
     # Scale factors to map function values to the display's pixel coordinates
     x_scale = (x_max - x_min) /(width)
     y_scale = (y_max - y_min) / (height)
@@ -112,6 +172,7 @@ def plot_function(fb, func, x_min, x_max, y_min, y_max, width, height):
             print(" pixel= ",x_pixel," ", y_pixel," value= ",x_value," ", y_value ," past= ",x_past," ",y_past)
             x_past=0
             y_past=0
+            # fb.vline(x_pixel,y_pixel, 100,1)
 
         # Ensure y_pixel is within the display range
         if 0 <= y_pixel < height:
@@ -122,19 +183,47 @@ def plot_function(fb, func, x_min, x_max, y_min, y_max, width, height):
                 fb.line(x_past, y_past, x_pixel-1, y_pixel-1, 1)
             x_past=x_pixel-1
             y_past=y_pixel-1
+    #numbering
+    fb.line(31,33,31,29,1)
+    fb.line(95,33,95,29,1)
+    fb.hline(62,0,4,1)
+    fb.hline(62,63,4,1)
+
 
 def polynom(x):
-    y=e**(2-0.2*x)
+    # y=sin(x)
+    # y=x*sin(x)
+    # y=e**x
+    # y=0
+    # if x in range[-1,1]:
+    #     y=asin(x)
+    # y=tan(x)
+    # if x<0:
+    #     y=-1
+    # if x>0:
+    #     y=1
+    if x == 0:
+        y=25
+    else:
+        y=sin(x)/x  
     return y
+
+# def polynom2(x):
+#     # y=sin(x)
+#     y=sin(x)
+#     return y
 
 fb.line(0,31,127,31, 1)
 fb.line(63,0,63,63,1)
 
-resolution=7
+# fb.text("3", 31, 31, 1)
+
+resolution=4
 x_res=2
 y_res=1
 
 plot_function(fb, polynom, -resolution*x_res, resolution*x_res, -resolution*y_res, resolution*y_res, 128, 64)
+
 print("")
 print("")
 binary_visual_list = bytearray_to_binary_visual_list(buffer)
@@ -150,3 +239,65 @@ def update_display():
 
 # # Example: after drawing, call update_display() to refresh the display with the current buffer
 update_display()
+
+set_page_address(4)
+set_column_address(26)
+for i in graph_letters["-"]:
+    write_data(i)
+# for i in graph_letters["2"]:
+#     write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+for i in graph_letters["R"]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+
+set_page_address(4)
+set_column_address(26+64)
+for i in graph_letters["+"]:
+    write_data(i)
+# for i in graph_letters["2"]:
+#     write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+for i in graph_letters["R"]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+
+set_page_address(0)
+set_column_address(55)
+for i in graph_letters["+"]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+for i in graph_letters["R"]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+
+set_page_address(7)
+set_column_address(55)
+for i in graph_letters["-"]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+for i in graph_letters["R"]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+
+set_page_address(7)
+set_column_address(112)
+for i in graph_letters["R"]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+for i in graph_letters["="]:
+    write_data(i)
+for i in graph_letters[" "]:
+    write_data(i)
+for i in graph_letters[str(resolution)]:
+    write_data(i)
+
